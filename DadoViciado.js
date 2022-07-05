fa4e914ec5c34028e8d420d6e27ef069583fef3b
Henrique Mateus Vargas dos Santos <108635477+DevHenriquecido@users.noreply.github.com>
2022-07-05 18:55:48 -0300

    Update DadoViciado.js

--- DadoViciado.js
+++ DadoViciado.js
@@ -31,54 +31,38 @@ function TestarDado(){
 for (let i = 0; dado.length < quantidades;i++){
 dado[i] = Math.floor(Math.random() * (7 - 1) + 1)
 if (dado[i] == 1){
-    let porcento1 = (contador1++ * 100) / quantidades
+    contador1++
+    let porcento1 = (contador1 * 100) / quantidades
     document.getElementById('div1').innerHTML = 'Número 1 Sorteado ' + contador1 + ' vezes e sua porcentagem foi de: ' + porcento1.toFixed(2) + '%'
-if (contador1 == 1 || contador1 == 2){
-    porcento1 = (contador1 *100) / quantidades
-    document.getElementById('div1').innerHTML = 'Número 1 Sorteado ' + contador1 + ' vezes e sua porcentagem foi de: ' + porcento1.toFixed(2) + '%'
-}}
+}
 if (dado[i] == 2){
-    let porcento2 = (contador2++ * 100) / quantidades
+    contador2++
+    let porcento2 = (contador2 * 100) / quantidades
     document.getElementById('div2').innerHTML = 'Número 2 Sorteado ' + contador2 + ' vezes e sua porcentagem foi de: ' + porcento2.toFixed(2) + '%'
-
-if (contador2 == 1 || contador2 == 2){
-    porcento2 = (contador2 *100) / quantidades
-    document.getElementById('div2').innerHTML = 'Número 2 Sorteado ' + contador2 + ' vezes e sua porcentagem foi de: ' + porcento2.toFixed(2) + '%'
-}}
+}
 
 if (dado[i] == 3){
-    let porcento3 = (contador3++ * 100) / quantidades
+    contador3++
+    let porcento3 = (contador3 * 100) / quantidades
     document.getElementById('div3').innerHTML = 'Número 3 Sorteado ' + contador3 + ' vezes e sua porcentagem foi de: ' + porcento3.toFixed(2) + '%'
-
-if (contador3 == 1 || contador3 == 2){
-    porcento3 = (contador3 *100) / quantidades
-    document.getElementById('div3').innerHTML = 'Número 3 Sorteado ' + contador3 + ' vezes e sua porcentagem foi de: ' + porcento3.toFixed(2) + '%'
-}}
+}
 
 if (dado[i] == 4){
-    let porcento4 = (contador4++ * 100) / quantidades
+    contador4++
+    let porcento4 = (contador4 * 100) / quantidades
     document.getElementById('div4').innerHTML = 'Número 4 Sorteado ' + contador4 + ' vezes e sua porcentagem foi de: ' + porcento4.toFixed(2) + '%'
-
- if (contador4 == 1 || contador4 == 2){
-    porcento4 = (contador4 *100) / quantidades
-    document.getElementById('div4').innerHTML = 'Número 4 Sorteado ' + contador4 + ' vezes e sua porcentagem foi de: ' + porcento4.toFixed(2) + '%'
-}}
+}
 
 if (dado[i] == 5){
-    let porcento5 = (contador5++ * 100) / quantidades
+    contador5
+    let porcento5 = (contador5 * 100) / quantidades
     document.getElementById('div5').innerHTML = 'Número 5 Sorteado ' + contador5 + ' vezes e sua porcentagem foi de: ' + porcento5.toFixed(2) + '%'
-
-if (contador5 == 1 || contador5 == 2){
-    porcento5 = (contador5 *100) / quantidades
-    document.getElementById('div5').innerHTML = 'Número 5 Sorteado ' + contador5 + ' vezes e sua porcentagem foi de: ' + porcento5.toFixed(2) + '%'
-}}
+}
 
 if (dado[i] == 6){
-    let porcento6 = (contador6++ * 100) / quantidades
+    contador6++
+    let porcento6 = (contador6 * 100) / quantidades
     document.getElementById('div6').innerHTML = 'Número 6 Sorteado ' + contador6 + ' vezes e sua porcentagem foi de: ' + porcento6.toFixed(2) + '%'
-
-if (contador6 == 1 || contador6 == 2){
-    porcento6 = (contador6 *100) / quantidades
-    document.getElementById('d0,iv6').innerHTML = 'Número 6 Sorteado ' + contador6 + ' vezes e sua porcentagem foi de: ' + porcento6.toFixed(2) + '%'
-}}}
+}}
 }
+
